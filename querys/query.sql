3830417001a6fd228adb19fb3ae3f0924e3f19da
chenaoh <ariza.ps4@gmail.com>
2024-08-26 11:27:21 -0600

    Mejoras

--- querys/query.sql
+++ querys/query.sql
@@ -4,20 +4,20 @@ use Hoteles;
 
 
 CREATE TABLE Usuarios (
-    CedulaID INT PRIMARY KEY,
+    CedulaID INT PRIMARY KEY AUTO_INCREMENT,
     Nombre VARCHAR(100) NOT NULL,
     Correo VARCHAR(100) UNIQUE NOT NULL
 );
 
 
 CREATE TABLE Administradores (
- AdministradorID INT PRIMARY KEY,
+ AdministradorID INT PRIMARY KEY AUTO_INCREMENT,
  Nombre VARCHAR(100) NOT NULL,
  Correo VARCHAR(100) UNIQUE NOT NULL
 );
 
 CREATE TABLE Hoteles (
-HotelID INT PRIMARY KEY,
+HotelID INT PRIMARY KEY AUTO_INCREMENT,
 Nombre VARCHAR(100) NOT NULL,
 Clasificacion INT,
 AdministradorID INT,
@@ -29,3 +29,4 @@ create TABLE Habitaciones (HabitacionID INT PRIMARY KEY AUTO_INCREMENT, HotelID 
 create TABLE Servicios (ServicioID INT PRIMARY KEY AUTO_INCREMENT, Nombre VARCHAR(100) NOT NULL, Costo DECIMAL(10, 2), HotelID INT, FOREIGN KEY (HotelID) REFERENCES Hoteles(HotelID))
 
 create TABLE Reservas (ReservaID INT PRIMARY KEY AUTO_INCREMENT, CedulaID INT, HabitacionID INT, FechaInicio DATE NOT NULL, FechaFin DATE NOT NULL, FOREIGN KEY (CedulaID) REFERENCES Usuarios(CedulaID), FOREIGN KEY (HabitacionID) REFERENCES Habitaciones(HabitacionID));
+
