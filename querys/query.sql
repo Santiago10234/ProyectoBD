e1dfb0c4302e22f8cd307e0f2ada1eb94279b2ad
chenaoh <ariza.ps4@gmail.com>
2024-08-26 11:21:08 -0600

    Creacion de tablas Habitaciones, Servicios, Reservas

--- querys/query.sql
+++ querys/query.sql
@@ -7,7 +7,6 @@ CREATE TABLE Usuarios (
     CedulaID INT PRIMARY KEY,
     Nombre VARCHAR(100) NOT NULL,
     Correo VARCHAR(100) UNIQUE NOT NULL
-
 );
 
 
@@ -24,3 +23,9 @@ Clasificacion INT,
 AdministradorID INT,
 Foreign Key (AdministradorID) REFERENCES Administradores(AdministradorID)
 );
+
+create TABLE Habitaciones (HabitacionID INT PRIMARY KEY, HotelID INT, NumeroHabitacion VARCHAR(10) NOT NULL, Disponibilidad BOOLEAN NOT NULL, Clasificacion VARCHAR(50), FOREIGN KEY (HotelID) REFERENCES Hoteles(HotelID));
+
+create TABLE Servicios (ServicioID INT PRIMARY KEY, Nombre VARCHAR(100) NOT NULL, Costo DECIMAL(10, 2), HotelID INT, FOREIGN KEY (HotelID) REFERENCES Hoteles(HotelID))
+
+create TABLE Reservas (ReservaID INT PRIMARY KEY, CedulaID INT, HabitacionID INT, FechaInicio DATE NOT NULL, FechaFin DATE NOT NULL, FOREIGN KEY (CedulaID) REFERENCES Usuarios(CedulaID), FOREIGN KEY (HabitacionID) REFERENCES Habitaciones(HabitacionID));
