4c9e36be746c125c63a17cd6a73c2c8e0bd86d94
kennvil102 <kennvilchez@gmail.com>
2024-08-26 11:06:05 -0600

    creacion tablas Usuario-administradores-Hoteles

--- querys/query.sql
+++ querys/query.sql
@@ -3,3 +3,24 @@ create DATABASE Hoteles;
 use Hoteles;
 
 
+CREATE TABLE Usuarios (
+    CedulaID INT PRIMARY KEY,
+    Nombre VARCHAR(100) NOT NULL,
+    Correo VARCHAR(100) UNIQUE NOT NULL
+
+);
+
+
+CREATE TABLE Administradores (
+ AdministradorID INT PRIMARY KEY,
+ Nombre VARCHAR(100) NOT NULL,
+ Correo VARCHAR(100) UNIQUE NOT NULL
+);
+
+CREATE TABLE Hoteles (
+HotelID INT PRIMARY KEY,
+Nombre VARCHAR(100) NOT NULL,
+Clasificacion INT,
+AdministradorID INT,
+Foreign Key (AdministradorID) REFERENCES Administradores(AdministradorID)
+);
